e4e205e675fd2a8817cdaa9224f0e66422332984
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-19 12:02:31 +0700

    feat: :sparkles: Setup tanstack router

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <main className='p-4 min-h-full'>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is where you can find the latest updates and news.</p>
+    </main>
+  )
+}
+
+export default Home
