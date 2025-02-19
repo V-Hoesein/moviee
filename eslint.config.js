faa94d371528cf585d6f47da60cbdf4371a135e9
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-19 10:38:52 +0700

    feat: :heavy_plus_sign: Setup tanstack query

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 import importNewlineAfter from 'eslint-plugin-import'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config(
   {
@@ -16,6 +17,7 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommended,
       prettierConfig,
+      ...pluginQuery.configs['flat/recommended'],
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Layout from '@/components/fragments/Layout'
 import { ThemeProvider } from './components/ThemeProvider'
 import { ModeToggle } from './components/ModeToggle'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <ThemeProvider>
-      <Layout>
-        <ModeToggle />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <ModeToggle />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
