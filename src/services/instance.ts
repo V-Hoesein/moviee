a60a9655aeafedf7e2f51de58a6c40f35a2b19b7
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-19 08:56:41 +0700

    perf: :truck: Change ext to .ts & change API KEY

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.VITE_RAPID_API_KEY}`,
   },
 })
 
