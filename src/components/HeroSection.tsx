af6d830cb06f64ff0e8c7e83a14c61f7d6b29915
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-21 02:15:52 +0700

    refactor: :art: update HeroSection and Header components for improved layout and styling

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,12 +2,12 @@ import { WavyBackground } from './ui/wavy-background'
 
 const HeroSection = () => {
   return (
-    <div id='hero' className={`h-[calc(100vh-20%)]`}>
+    <div id='hero' className={`h-[calc(100vh-20%)] -z-10`}>
       <WavyBackground className='max-w-4xl mx-auto pb-40' speed='slow'>
         <p className='text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center'>
           Welcome to movie app
         </p>
-        <p className='text-lg md:text-lg mt-4 text-white font-normal inter-var text-center'>
+        <p className='text-lg md:text-xl lg:text-2xl mt-4 text-white font-normal inter-var text-center'>
           Find your favorite movie and watch it now
         </p>
       </WavyBackground>
