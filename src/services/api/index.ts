ba912376fbbdd77a7af982d5c04d90958f8872fa
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-20 13:36:45 +0700

    perf: :art: Added some fetch functions

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,24 @@
 import instance from '@/services/instance'
+import { GetListResponse } from '@/types/globals'
+import { GetPopularMoviesProps, GetTrendingsProps } from '@/types/movie'
+import { GetTvPopularProps } from '@/types/tv'
 
-const getGenres = async (): Promise<string[]> => await instance.get('/genres')
+const getTrendings = async ({
+  type = 'all',
+  category = 'day',
+}: GetTrendingsProps): Promise<GetListResponse> =>
+  await instance.get(`/trending/${type}/${category}?language=en-US`)
 
-export { getGenres }
+const getPopularMovie = async ({
+  language,
+  page = 1,
+}: GetPopularMoviesProps): Promise<GetListResponse> =>
+  await instance.get(`/movie/popular?language=${language}&page=${page}`)
+
+const getPopularTv = async ({
+  type,
+  page = 1,
+}: GetTvPopularProps): Promise<GetListResponse> =>
+  await instance.get(`/tv/${type}?language=en-US&page=${page}`)
+
+export { getTrendings, getPopularTv, getPopularMovie }
