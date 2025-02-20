83ef165038480a2097260cc876617ae09363ad94
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-20 17:51:41 +0700

    refactor: :lipstick: remove wrapper, use fragment

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -3,19 +3,19 @@ import { Skeleton } from './ui/skeleton'
 
 const SkeletonLoader = ({
   className,
-  count = 5,
+  count = 3,
 }: {
   className?: string
   count?: number
 }) => (
-  <div className='flex mt-3 space-x-2 w-full'>
+  <>
     {[...Array(count)].map((_, i) => (
       <Skeleton
         key={i}
         className={`${cn('h-52 w-2/6 min-w-[150px] rounded-md', className)}`}
       />
     ))}
-  </div>
+  </>
 )
 
 export default SkeletonLoader
