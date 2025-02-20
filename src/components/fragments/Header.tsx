fb52331b05cd5c1868e58f61c1694ea52b945787
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-21 02:59:46 +0700

    refactor: :art: enhance Header component with framer-motion for improved entrance animation

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -1,12 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import Navigation from '../Navigation'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   return (
-    <div
-      className='fixed top-0 left-0 right-0 z-50 bg-gradient-to-b
-         from-zinc-950 via-zinc-950/70 to-zinc-950/20 shadow-sm
-          backdrop-blur-md p-5 sm:p-10 w-full'
+    <motion.div
+      className='fixed top-0 left-0 right-0 z-50 bg-gradient-to-b from-zinc-950 via-zinc-950/70 to-zinc-950/20 shadow-sm backdrop-blur-md p-5 sm:p-10 w-full'
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6, ease: 'easeOut' }}
     >
       <header className='mx-auto md:max-w-5xl space-y-5'>
         <div className='flex justify-between items-center'>
@@ -23,7 +25,7 @@ const Header = () => {
           <Navigation />
         </div>
       </header>
-    </div>
+    </motion.div>
   )
 }
 
