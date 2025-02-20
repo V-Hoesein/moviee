780b938ce11d1595d86596261077271f3ebada76
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-21 01:25:44 +0700

    refactor: :art: remove search form from header

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -1,22 +1,24 @@
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import Navigation from '../Navigation'
-import SearchForm from '../SearchForm'
 
 const Header = () => {
   return (
     <>
       {/* mobile */}
-      <header className='fixed top-0 left-0 right-0 z-50 bg-gradient-to-b from-zinc-950 via-zinc-950/70 to-zinc-950/20 shadow-sm backdrop-blur-xs'>
-        <div className='flex justify-between items-center p-4'>
+      <header
+        className='fixed top-0 left-0right-0 z-50 bg-gradient-to-b
+         from-zinc-950 via-zinc-950/70 to-zinc-950/20 shadow-sm
+          backdrop-blur-xs p-5 w-full space-y-5'
+      >
+        <div className='flex justify-between items-center'>
           <h1 className='capitalize font-bold text-2xl'>moviee</h1>
           <Avatar>
             <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </div>
-        <div className='flex justify-between items-center px-5 pb-2 gap-3'>
+        <div className='flex justify-between items-center pb-2 gap-3'>
           <Navigation />
-          <SearchForm />
         </div>
       </header>
     </>
