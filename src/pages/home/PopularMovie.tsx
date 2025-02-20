759a71531614727a8c44b290c2dcdd7527fdf18e
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-20 17:54:35 +0700

    perf: :recycle: add next & prev buttons and configure mutation

--- src/pages/home/PopularMovie.tsx
+++ src/pages/home/PopularMovie.tsx
@@ -1,32 +1,95 @@
 import ContentSection from '@/components/ContentSection'
 import { MovieContainer } from '@/components/MovieContainer'
+import { Button } from '@/components/ui/button'
 import { getListContent } from '@/services/api'
+import { GetListResponse } from '@/types/globals'
 import { GetPopularMoviesProps } from '@/types/movie'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { MoveLeft, MoveRight } from 'lucide-react'
 import { useState } from 'react'
 
 const PopularMovie = () => {
-  const [popularMovie] = useState<GetPopularMoviesProps>({
+  const [popularMovie, setPopularMovie] = useState<GetPopularMoviesProps>({
     language: 'en-US',
     page: 1,
   })
 
-  const popularMovieList = useQuery({
+  const queryClient = useQueryClient()
+
+  const { data, isFetching } = useQuery({
     initialData: { results: [], page: 1, total_pages: 1, total_results: 0 },
     queryKey: ['popularMovie', popularMovie],
-    queryFn: () => getListContent({ type: 'movie', category: 'popular' }),
+    queryFn: () =>
+      getListContent({
+        type: 'movie',
+        category: 'popular',
+        page: popularMovie.page,
+      }),
     refetchOnWindowFocus: false,
   })
 
+  const mutation = useMutation<GetListResponse, Error, number>({
+    mutationFn: (newPage) =>
+      getListContent({ type: 'movie', category: 'popular', page: newPage }),
+
+    onSuccess: (newData, newPage) => {
+      queryClient.setQueryData(['popularMovie', newPage], newData)
+
+      setPopularMovie((prev) => ({
+        ...prev,
+        page: newPage,
+      }))
+    },
+  })
+
+  const handleNext = () => {
+    if (!mutation.isPending) {
+      mutation.mutate(popularMovie.page + 1)
+    }
+  }
+
+  const handlePrev = () => {
+    if (popularMovie.page > 1 && !mutation.isPending) {
+      mutation.mutate(popularMovie.page - 1)
+    }
+  }
+
+  console.log(data)
+
   return (
-    <>
-      <ContentSection sectionName='popular movie'>
+    <ContentSection sectionName='Popular Movies'>
+      <div className='flex overflow-x-auto items-center'>
+        {data.page !== 1 && (
+          <div className='px-4'>
+            <Button
+              className='rounded-full'
+              variant='outline'
+              size='icon'
+              onClick={handlePrev}
+              disabled={popularMovie.page === 1}
+            >
+              <MoveLeft />
+            </Button>
+          </div>
+        )}
         <MovieContainer
-          data={popularMovieList.data.results}
-          isFetching={popularMovieList.isFetching}
+          data={data.results}
+          isFetching={isFetching || mutation.isPending}
         />
-      </ContentSection>
-    </>
+        {data.page !== data.total_pages && (
+          <div className='px-4'>
+            <Button
+              className='rounded-full'
+              variant='outline'
+              size='icon'
+              onClick={handleNext}
+            >
+              <MoveRight />
+            </Button>
+          </div>
+        )}
+      </div>
+    </ContentSection>
   )
 }
 
